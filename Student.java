c5019e929053417595fb2b1733be290e9cd2d8bc
Ravensfolly <49001186+Ravensfolly@users.noreply.github.com>
2019-04-02 11:49:40 -0400

    Add files via upload

--- Student.java
+++ Student.java
@@ -19,5 +19,25 @@ public class Student
 		this.enrolled = enrolled;
 		this.completed = completed;
 	}
+	
+	/**
+	 * Enrolls the student in a given course by adding the course onto their list of enrolled courses.
+	 * @param acourse
+	 */
+	//public void enroll(Course acourse)
+	
+	/**
+	 * Finds the course in the students enrolled courses and changes the grade of that course as long as it is found.
+	 * If the course does not exist, then CourseNotFoundException applies.
+	 * @param acourse
+	 */
+	//public void changeGrade(Course acourse)
+	
+	/**
+	 * Moves all of the courses that the student is currently enrolled in into the completed course list, finalizing their
+	 * grade in that class in the process.
+	 */
+	//public void rollGrades()
+	
 
 }
--- StudentList.java
+++ StudentList.java
@@ -13,5 +13,19 @@ public class StudentList
 	{
 		this.students = students;
 	}
+	
+	/**
+	 * Adds a student onto the list of students, checking to make sure the that student does not already exist.
+	 * If the student does already exist then StudentAlreadyExistsException should apply.
+	 * @param astudent
+	 */
+	//public void addStudent(Student astudent)
+	
+	/**
+	 * Removes a given student(stated by their ID number) from the list of students as long as the student does exist.
+	 * If the student does not exist then StudentNotFoundException should apply.
+	 * @param numID
+	 */
+	//public void deleteStudent(int numID)
 
 }
